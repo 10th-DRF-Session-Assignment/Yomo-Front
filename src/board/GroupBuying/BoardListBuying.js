bf84db2ab70321964d063ae40096b843921da7de
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-25 00:57:31 +0900

    공구 페이지 글 목록 작성하기

--- src/board/GroupBuying/BoardListBuying.js
+++ src/board/GroupBuying/BoardListBuying.js
@@ -1,11 +1,9 @@
-import BoardItem from "./BoardItem";
-import App from "./App.css";
+import BoardItem from "./BoardItemBuying";
 
-const BoardList = ({boardList}) => {
+const BoardList = ({boardList}) => { 
     console.log(boardList);
     return (
         <div className='BoardList'>
-            <h2>기숙사 공구 게시판</h2>
             {/* <h4>글이 {boardList.length}개 있습니다.</h4> */}
             <div className="BoardBlock">
                 {boardList.map((it) => (
